e7feb349fa3824a0524b1306b30f490b0074ee5b
Sabrina Schekerka <sabrinaschekerka@gmail.com>
2022-09-06 12:42:41 +0200

    create two pages Veggie and Dessert

---
+++ pages/api/desserts.js
@@ -0,0 +1,9 @@
+
+
+export default function Desserts() {
+    return (
+    <>
+        <h2>Desserts Recipes</h2>
+        <p>Here comes a list of desserts recipes</p>
+    </>)
+}
---
+++ pages/api/veggie.js
@@ -0,0 +1,9 @@
+
+
+export default function Veggie() {
+    return (
+    <>
+        <h2>Veggie Recipes</h2>
+        <p>Here comes a list of veggie recipes</p>
+    </>)
+}
