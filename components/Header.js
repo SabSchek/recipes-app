e3494df4de2815356e0bac32eb6189822c5066f8
Sabrina Schekerka <sabrinaschekerka@gmail.com>
2022-09-06 20:31:01 +0200

    add components file with footer, header and Layout

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return  (
+        <Header>
+            <nav>
+                <a>Home</a>
+                <a>Veggie</a>
+                <a>Desserts</a>
+                </nav>
+                </header>
+    )
+}
